d402627ae2aa7e9cd603bba443f09590af43ba44
JK <49369341+iJKTen@users.noreply.github.com>
2021-01-27 12:13:46 -0500

    updated blog title

--- music-from/lib/data/index.js
+++ music-from/lib/data/index.js
@@ -1,8 +1,8 @@
 const metaData = {
     title: "Music My Friend Jai likes",
     description:
-        "An attempt to recreate aurgasm.us by My Jai Friend",
-    url: "https://next-basic-blog.vercel.app",
+        "An attempt to recreate aurgasm.us by My Friend Jai",
+    url: "https://musicfrom.myfriendjai.com",
     image: "/assets/meta/blogSimple.jpg",
 };
 
